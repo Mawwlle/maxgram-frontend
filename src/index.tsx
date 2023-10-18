ca66b0ab23677eaaaf3fa705e3df0ee43fba897d
Backwards <deema.light@gmail.com>
2023-10-18 18:01:36 +0300

    feat: add auto renew token

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Routes, Navigate, BrowserRouter } from 'react-router-dom';
-import { Route } from 'react-router';
+import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { throttle } from 'lodash';
 import Settings from './pages/settings';
 import Sidebar from './widgets/sidebar';
 // import Header from './widgets/header';
@@ -15,7 +15,7 @@ import PostModal from './widgets/postModal';
 import { useEffect, useState } from 'react';
 import api from './api';
 import { useAppDispatch, useAppSelector } from './store/hooks';
-import { set } from './store/user';
+import { remove, set } from './store/user';
 
 const AppRouter = () => {
   const modal = useAppSelector((store) => store.modal);
@@ -51,17 +51,51 @@ const App = () => {
   const dispatch = useAppDispatch();
   const isAuth = useAppSelector((store) => store.user.isAuth);
   const [isLoadingUserInfo, setIsLoadingUserInfo] = useState<boolean>(true);
+  
+  const requestAuth = async() => {
+    try {
+      await api.users.getSelfUser()
+        .then((resp) => {
+          dispatch(set(resp.data));
+          setIsLoadingUserInfo(false);
+        });
+    } catch (err) {
+      console.warn(err);
+      setIsLoadingUserInfo(false);
+    }
+  }
+
+  const renewToken = () => {
+    const refreshToken = localStorage.getItem('refresh_token');
+    if (refreshToken)
+    api.token.refreshToken(refreshToken)
+      .then((resp) => {
+        localStorage.setItem('access_token', resp.data.access);
+      });
+  };
+
+  const throttledRenewToken = throttle(renewToken, 250000, { trailing: false });
 
   useEffect(() => {
-    api.users.getSelfUser()
-      .then((resp) => {
-        dispatch(set(resp.data));
-        setIsLoadingUserInfo(false);      
-      })
-      .catch(() => {
-        setIsLoadingUserInfo(false);
-      })
-  }, []);
+    const refreshToken = localStorage.getItem('refresh_token');
+    if (refreshToken) {
+      throttledRenewToken();
+      setTimeout(() => requestAuth(), 300);
+    
+      const renewalInterval = setInterval(() => {
+        throttledRenewToken();
+      }, 250000);
+  
+      return () => {
+        clearInterval(renewalInterval);
+      };
+    } else {
+      dispatch(remove());
+      setIsLoadingUserInfo(false);
+
+      return () => void{};
+    }
+  }, [isAuth]);
 
   if (isLoadingUserInfo) {
     return (
